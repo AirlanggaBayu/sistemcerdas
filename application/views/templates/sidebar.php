fd8a7f53693cb16f7ecb74792c02ab7630f62aad
AirlanggaBayu <masbaay7@gmail.com>
2021-06-11 09:04:36 +0700

    ngatur template

---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,53 @@
+<div class="sidebar" data-color="blue">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          Deteksi Kanker Payudara
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="./dashboard.html">
+              <i class="fa fa-user-md" aria-hidden="true"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a href="./icons.html">
+              <i class="now-ui-icons education_atom"></i>
+              <p>Diagnosa</p>
+            </a>
+          </li>
+          <li>
+            <a href="./map.html">
+              <i class="fa fa-heartbeat" aria-hidden="true"></i>
+              <p>Gejala</p>
+            </a>
+          </li>
+          <li>
+            <a href="./notifications.html">
+              <i class="fa fa-medkit" aria-hidden="true"></i>
+              <p>Pengetahuan</p>
+            </a>
+          </li>
+          <li>
+            <a href="./user.html">
+              <i class="fa fa-stethoscope" aria-hidden="true"></i>
+              <p>Konsultasi</p>
+            </a>
+          </li>
+         
+         
+          <li class="active-pro">
+            <a href="./upgrade.html">
+              <i class="fas fa-sign-out-alt"></i>
+              <p>Keluar</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
